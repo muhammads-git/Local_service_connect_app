282fe3d7c9fc01cfd5cfbfef44a00a0a3778b913
muhammads-git <roadprogrammar@gmail.com>
2025-10-22 15:59:07 +0500

    Top Rated Service prviders for user_dashboard (fetched*)

--- app/routess/dashboards.py
+++ app/routess/dashboards.py
@@ -20,7 +20,7 @@ def user_dashboard():
         sp.username, 
         sp.profession,
         sp.profession_desc,
-        AVG(r.rating) as avg_rating  -- Use AVG to get single rating per provider
+        ROUND(AVG(r.rating),1) as avg_rating  -- Use AVG to get single rating per provider
     FROM service_providers sp
     JOIN reviews r ON sp.id = r.provider_id
     GROUP BY sp.id, sp.username, sp.profession, sp.profession_desc
@@ -28,25 +28,8 @@ def user_dashboard():
    """)
    service_providers_data = cursor.fetchall()
    cursor.close()
-
-   # get the top rated
-   cursor =mysql.connection.cursor()
-   cursor.execute("""
-   SELECT 
-                  sp.username,
-                  sp.profession,
-                  CAST(AVG(r.rating) AS DECIMAL(3,1)) as avg_rating
-         FROM service_providers sp
-         JOIN reviews r ON sp.id = r.provider_id
-         GROUP BY sp.username, sp.profession
-         ORDER BY avg_rating DESC
-         LIMIT 1
-   """)
-   top_rated = cursor.fetchall()
-
-
-
-   return render_template('dashboards/user_dashboard.html',username=session['username'],service_providers_data=service_providers_data,top_rated=top_rated)
+      
+   return render_template('dashboards/user_dashboard.html',username=session['username'],service_providers_data=service_providers_data)
 
 @dashboards_bp.route('/book_service',methods=['GET','POST'])
 def book_service():
