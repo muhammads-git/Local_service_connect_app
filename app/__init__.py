83ed5c4c88eaf2317689ad94bbe6cc62dc418c07
muhammads-git <roadprogrammar@gmail.com>
2026-01-10 14:00:45 +0500

    debugg 1

--- app/__init__.py
+++ app/__init__.py
@@ -14,36 +14,36 @@ csrf = CSRFProtect()
 app = Flask(__name__)
 
 
-@app.before_request
-def fetch_notifications():    
-   if 'user_id' in session or 'provider_id' in session:
-      cursor = mysql.connection.cursor()
-      # User or Provider
-      recipient_id = session.get('user_id') or session.get('provider_id')
+# @app.before_request
+# def fetch_notifications():    
+#    if 'user_id' in session or 'provider_id' in session:
+#       cursor = mysql.connection.cursor()
+#       # User or Provider
+#       recipient_id = session.get('user_id') or session.get('provider_id')
       
-      try:
-         cursor.execute(""" SELECT
-            job_id,  
-            COUNT(*) as message_count, 
-            MAX(created_at) as latest_time, 
-            GROUP_CONCAT(message SEPARATOR ' | ') as messages_combined,
-            MIN(is_read) as unread
-            FROM notifications 
-            WHERE recipient_id = %s 
-            GROUP BY job_id
-            ORDER BY latest_time DESC """ ,(recipient_id,))
-         notifications = cursor.fetchall()
+#       try:
+#          cursor.execute(""" SELECT
+#             job_id,  
+#             COUNT(*) as message_count, 
+#             MAX(created_at) as latest_time, 
+#             GROUP_CONCAT(message SEPARATOR ' | ') as messages_combined,
+#             MIN(is_read) as unread
+#             FROM notifications 
+#             WHERE recipient_id = %s 
+#             GROUP BY job_id
+#             ORDER BY latest_time DESC """ ,(recipient_id,))
+#          notifications = cursor.fetchall()
 
-      except Exception as e:
-         flash(f"error {e} occured, try again!",'warning')
+#       except Exception as e:
+#          flash(f"error {e} occured, try again!",'warning')
 
-      if not notifications:
-         notifications = 0   #default   
-      unread_count = sum(1 for note in notifications if note[4] == 0)
+#       if not notifications:
+#          notifications = 0   #default   
+#       unread_count = sum(1 for note in notifications if note[4] == 0)
 
-      # make available for all the routes
-      g.unread_count = unread_count
-      g.notifications = notifications                  
+#       # make available for all the routes
+#       g.unread_count = unread_count
+#       g.notifications = notifications                  
 
 # creating app
 def create_app():
